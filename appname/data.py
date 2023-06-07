75748a8e47889da9f1a3cb156b5228f7311fba07
MithiJ <mithi_jethwa@brown.edu>
2023-06-06 21:13:34 -0400

    defensive copying added

---
+++ appname/data.py
@@ -0,0 +1,10 @@
+class Data:
+    def __init__(self, initial_data):
+        self.data = initial_data.copy()
+        # This should actually take in the parameters and load data from data store
+
+    def get_data(self):
+        return self.data.copy()
+    
+    def set_data(self, new_data):
+        self.data = new_data.copy()
--- appname/views.py
+++ appname/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .data import Data
 
-# Create your views here.
-data = [ ] # TODO: What data structure will we get the dataframes in? Array of array?
+# Create your views here
+data = Data([])
 
 def load_dataframe(request):
     global data
     backend_data = [1, 2, 3] # Sample data
     # get the data from backend datastore
-    data = backend_data
-    return JsonResponse({'result': 'Data loaded', 'data': data})
+    data.set_data(backend_data)
+    return JsonResponse({'result': 'Data loaded', 'data': data.get_data()})
 
 def perform_analysis(request):
-    analyzer_func(data) 
+    new_data = analyzer_func(data.get_data()) 
+    data.set_data(new_data)
     """
     Here, we can copy over the dataframe in order to not amend it. Likely,
     we should have getters and setters for the data in a separate file that are
     imported in and leave the data as a private variable in that class.
     """
-    return JsonResponse({'result': 'analysis performed', 'data': data})
+    return JsonResponse({'result': 'analysis performed', 'data': data.get_data()})
 
 def analyzer_func(some_data):
     for i in range(len(some_data)):
         some_data[i] += 1
+    return some_data
