cd8844a3c4e93c685410eab86a6aaa5853f3475c
MithiJ <mithi_jethwa@brown.edu>
2023-06-21 21:23:14 -0400

    Memcached option added

---
+++ flask/memc.py
@@ -0,0 +1,19 @@
+from flask import Flask, session, request
+from flask_session import Session
+
+app = Flask(__name__)
+# Check Configuration section for more details
+SESSION_TYPE = 'memcached'
+app.config.from_object(__name__)
+Session(app)
+
+@app.route('/set')
+def set():
+    filepath = request.args.get('filepath')
+    # URL: http://127.0.0.1:5000/set?filepath=xyz
+    session['key'] = str(filepath)
+    return 'ok'
+
+@app.route('/get/')
+def get():
+    return session.get('key', 'not set')
